9209cdb093a8d38c5cd2863c5b3ca81a027b5db9
Ruslan <ruslanberdyiev@gmail.com>
2020-12-30 18:52:23 +0100

    Created modal window

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -1,6 +1,6 @@
 
-import React from 'react';
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Navbar, Nav, Button, Container, Form, Modal } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -14,6 +14,12 @@ const Styles = styled.div `
 `
 
 const NaviBar= () => {
+
+    const [show,setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
      <>
       <Styles>
@@ -28,15 +34,36 @@ const NaviBar= () => {
                     <Nav.Link><Link to='/contacts'>Contacts</Link></Nav.Link>
                 </Nav>
                 <Nav>
-                    <Button variant='primary' className='mr-2'>Log in</Button>
-                    <Button variant='primary'>Sign out</Button>
+                    <Button variant='primary' className='mr-2' onClick={handleShow}>Log in</Button>
+                    <Button variant='primary' onClick={handleShow}>Sign out</Button>
                 </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
       </Styles>
+      <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+                <Modal.Title>
+                    Log in
+                </Modal.Title>
+                <Modal.Body>
+                        <Form.Group controlId='fromBasicEmail'>
+                            <Form.Label>Email Adress</Form.Label>
+                            <Form.Control type='email' placeholder='Enter email'/>
+                            <Form.Text className="text-muted">We`ll never share your email</Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId='fromBasicPassword'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' placeholder='Enter email'/>
+                        </Form.Group>
+                        <Form.Group controlId='fromBasicCheckbox'>
+                            <Form.Check type='checkbox' label='Remember me'/>
+                        </Form.Group>
+                </Modal.Body>
+            </Modal.Header>
+      </Modal>
      </>
-    )
+    );
 }
 
 export default NaviBar;
